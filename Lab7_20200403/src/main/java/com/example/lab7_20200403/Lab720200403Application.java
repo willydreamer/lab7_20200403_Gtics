e04900c3530d8d907365dc63680376c0607bc1e8
Willy Huallpa <willy.huallpa@pucp.edu.pe>
2023-09-28 20:28:57 -0500

    proyecto creado

---
+++ Lab7_20200403/src/main/java/com/example/lab7_20200403/Lab720200403Application.java
@@ -0,0 +1,13 @@
+package com.example.lab7_20200403;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab720200403Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab720200403Application.class, args);
+    }
+
+}
---
+++ Lab7_20200403/src/test/java/com/example/lab7_20200403/Lab720200403ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab7_20200403;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab720200403ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
